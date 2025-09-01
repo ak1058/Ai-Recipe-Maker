9359a86c402e87e256e2d1bcc0d9f953ae9c0b17
ak1058 <amit.ak.ak933@gmail.com>
2025-09-01 11:26:06 +0530

    added middleware to allow auth header

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.routes import user, inventory, recipe  
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],  
+)
 
 app.include_router(user.router)
 app.include_router(inventory.router)
